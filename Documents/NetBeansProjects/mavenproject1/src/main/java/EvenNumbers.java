4b61d6075fbeb6a4fd32dfd497f2564cd89887a2
meena538 <srigurumena18@gmail.com>
2020-03-01 13:28:34 +0530

    1st commit

---
+++ Documents/NetBeansProjects/mavenproject1/src/main/java/EvenNumbers.java
@@ -0,0 +1,22 @@
+// Java Program to Print Even Numbers from 1 to N using For loop
+import java.util.Scanner;
+
+public class EvenNumbers {
+	private static Scanner sc;
+	public static void main(String[] args) 
+	{
+		int number, i;
+		sc = new Scanner(System.in);
+		
+		System.out.print(" Please Enter any Number : ");
+		number = sc.nextInt();	
+		
+		for(i = 1; i <= number; i++)
+		{
+			if(i % 2 == 0)
+			{
+				System.out.print(i +"\t"); 
+			}
+		}	
+	}
+}
